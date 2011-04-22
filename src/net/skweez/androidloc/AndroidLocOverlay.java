ae58c9030a11dce7f44871febdda8d935cda1177
Michael Kanis <mks@skweez.net>
2011-04-22 22:08:32 +0200

    Remove empty default constructor

--- src/net/skweez/androidloc/AndroidLocOverlay.java
+++ src/net/skweez/androidloc/AndroidLocOverlay.java
@@ -5,17 +5,11 @@ import net.skweez.geoclipse.map.Overlay;
 
 import org.eclipse.swt.graphics.Color;
 import org.eclipse.swt.graphics.GC;
-import org.eclipse.swt.graphics.Rectangle;
 
 public class AndroidLocOverlay extends Overlay {
 
-	public AndroidLocOverlay() {
-	}
-
 	@Override
 	public void draw(GC gc, MapView mapView, boolean shadow) {
-		Rectangle clip = gc.getClipping();
-
 		for (int i = 0; i < 1; i++) {
 			gc.setBackground(new Color(gc.getDevice(), 255, 100, 0));
 			gc.setAlpha(100);
@@ -24,5 +18,5 @@ public class AndroidLocOverlay extends Overlay {
 			gc.setAlpha(255);
 			gc.drawOval(i * 3 + 100, 150, 50, 50);
 		}
-	};
+	}
 }
